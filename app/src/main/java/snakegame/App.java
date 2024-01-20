724e96cd5a249cb0d5c4d40c8a244b64384478e1
Owen McVey <owen.mcvey@proton.me>
2024-01-20 16:17:43 -0500

    Basic Window App creates a window using JFrame that is 1280 x 720 pixels in size

--- app/src/main/java/snakegame/App.java
+++ app/src/main/java/snakegame/App.java
@@ -3,12 +3,32 @@
  */
 package snakegame;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
+
+    public App() {
+        // Create the main game window
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0, 1));
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Snake");
+        frame.pack();
+        frame.setVisible(true);
+        frame.setSize(1280, 720);
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        new App();
     }
 }
